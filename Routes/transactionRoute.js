5fd693f0f1fb75a59091bda6f329b9e0795ca1ad
jest-in <jestin9900767795@gmail.com>
2023-08-19 10:44:22 +0530

    Authentication error solved

--- Routes/transactionRoute.js
+++ Routes/transactionRoute.js
@@ -6,33 +6,62 @@ const router = express.Router();
 // Public
 router.route("/user").get(Transaction.getUser);
 
-router.route(AuthController.protect);
+// router.route(AuthController.protect);
 
 // Admin
 router
   .route("/add")
-  .post(AuthController.restrictTo("admin"), Transaction.addTransaction);
+  .post(
+    AuthController.protect,
+    AuthController.restrictTo("admin"),
+    Transaction.addTransaction
+  );
 
 // Super-Admin/Admin
 router
   .route("/view-added")
   .get(
+    AuthController.protect,
     AuthController.restrictTo("super-admin", "admin"),
     Transaction.getAddedTransactions
   );
 
 // Super-Admin
-router.route(AuthController.restrictTo("super-admin"));
-router.route("/user-details").get(Transaction.getTransaction);
-router.route("/approve").patch(Transaction.approveTransaction);
+// router.route(AuthController.restrictTo("super-admin"));
+router
+  .route("/user-details")
+  .get(
+    AuthController.protect,
+    AuthController.restrictTo("super-admin"),
+    Transaction.getTransaction
+  );
+router
+  .route("/approve")
+  .patch(
+    AuthController.protect,
+    AuthController.restrictTo("super-admin"),
+    Transaction.approveTransaction
+  );
 router
   .route("/reward-eleigibles")
-  .get(AuthController.protect, Transaction.rewardableMembers);
+  .get(
+    AuthController.protect,
+    AuthController.restrictTo("super-admin"),
+    Transaction.rewardableMembers
+  );
 router
   .route("/reward")
-  .patch(AuthController.protect, Transaction.rewardTransaction);
+  .patch(
+    AuthController.protect,
+    AuthController.restrictTo("super-admin"),
+    Transaction.rewardTransaction
+  );
 router
   .route("/rewarded-transactions")
-  .get(AuthController.protect, Transaction.viewRewardedTransaction);
+  .get(
+    AuthController.protect,
+    AuthController.restrictTo("super-admin"),
+    Transaction.viewRewardedTransaction
+  );
 
 module.exports = router;
